1ce985c2f0ae3a37b369a3896cce72d085607dde
unknown <s297875@studenti.polito.it>
2024-05-08 17:20:23 +0200

    Third commit

--- Project/src/Data.hpp
+++ Project/src/Data.hpp
@@ -12,13 +12,13 @@ struct DFN
     unsigned int NumberFractures = {};
     vector<unsigned int> FracturesId = {};
     vector<unsigned int> NumberVertices = {};
-    map<unsigned int, Matrix<double, 3, Dynamic>> Vertices ={};
+    map<unsigned int, Matrix<double, 3, 4>> Vertices ={};
 
 
     unsigned int NumberTraces = 0;
     vector<unsigned int> TracesId = {};
     map<unsigned int, vector<unsigned int>> GeneratingFractures = {};
-    map<unsigned int, Matrix<double, 3,2>> GeneratingVertices = {};
+    map<unsigned int, Matrix<double, 3,4>> GeneratingVertices = {};
 
     vector<bool> Tips = {};
     vector<double> LenghtTraces = {};
--- Project/src/Utils.cpp
+++ Project/src/Utils.cpp
@@ -33,13 +33,12 @@ bool ImportAll(const string &filename, DFN &data)
     }
 
     string line;
-    char coma;
-    unsigned int id = 0;
-    unsigned int vertices = 0;
+
+
     unsigned int NFractures = 0;
     bool end = false;
 
-    istringstream convertN;
+    istringstream convertN(line);
 
 
     getline(file, line);
@@ -51,17 +50,27 @@ bool ImportAll(const string &filename, DFN &data)
 
     for(unsigned int i = 0; i != NFractures; i++)
     {
+        unsigned int id;
+        unsigned int vertices;
+        char tmp;
+        istringstream convertN(line);
+
         getline(file, line);
         cout << line << endl;
-        convertN.str(line);
-        cout << line << endl;
-        convertN >> id >> coma >> vertices;
+
+        convertN >> id >> tmp >> vertices;
         data.FracturesId.push_back(id);
         data.NumberVertices.push_back(vertices);
         getline(file, line);
         getline(file, line);
+
+        convertN >> data.Vertices(0,0) >> data.Vertices(0,1) >> data.Vertices(0,2) >> data.Vertices(0,3);
         getline(file, line);
+
+        convertN >> data.Vertices(1,0) >> data.Vertices(1,1) >> data.Vertices(1,2) >> data.Vertices(1,3);
         getline(file, line);
+
+        convertN >> data.Vertices(2,0) >> data.Vertices(2,1) >> data.Vertices(2,2) >> data.Vertices(2,3);
         getline(file, line);
 
     }
